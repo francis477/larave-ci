570bfb8248b37d1cac51b494d83d7304ddd42c51
francis477 <francisteye477@gmail.com>
2022-04-25 14:42:40 +0000

    update on action

--- deploy.php
+++ deploy.php
@@ -1,66 +1,45 @@
 <?php
-
 namespace Deployer;
 
 require 'recipe/laravel.php';
-// require 'recipe/rsync.php';
-
-set('application', 'My App');
-set('ssh_multiplexing', true);
-
-// set('rsync_src', function () {
-//     return __DIR__;
-// });
 
 
-// add('rsync', [
-//     'exclude' => [
-//         '.git',
-//         '/.env',
-//         '/storage/',
-//         '/vendor/',
-//         '/node_modules/',
-//         '.github',
-//         'deploy.php',
-//     ],
-// ]);
 
+set('application', 'dep-demo');
+set('ssh_multiplexing', true); // Speed up deployment
+
+
+// Set up a deployer task to copy secrets to the server. 
+// Grabs the dotenv file from the github secret
 task('deploy:secrets', function () {
     file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
     upload('.env', get('deploy_path') . '/shared');
 });
+// Hosts
+host('test.slymdev.website/') // Name of the server
+    ->hostname('[162.0.232.38]:21098') // Hostname or IP address
+    ->stage('staging') // Deployment stage (production, staging, etc)
+    ->user('deploy') // SSH user
+    ->set('deploy_path', '/cd/test.slymdev.website/');   // Deploy path
 
-// host('myapp.io')
-//   ->hostname('104.248.172.220')
-//   ->stage('production')
-//   ->user('root')
-//   ->set('deploy_path', '/var/www/my-app');
-
-host('test.slymdev.website')
-//   ->hostname('104.248.172.220')
-  ->stage('staging')
-//   ->user('root')
-  ->set('deploy_path', 'cd/test.slymdev.website');
-
-after('deploy:failed', 'deploy:unlock');
+    after('deploy:failed', 'deploy:unlock'); // Unlock after failed deploy
 
 desc('Deploy the application');
-
 task('deploy', [
     'deploy:info',
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
-    'rsync',
-    'deploy:secrets',
+    'rsync', // Deploy code & built assets
+    'deploy:secrets', // Deploy secrets
     'deploy:shared',
     'deploy:vendors',
     'deploy:writable',
-    'artisan:storage:link',
-    'artisan:view:cache',
-    'artisan:config:cache',
-    // 'artisan:migrate',
-    'artisan:queue:restart',
+    'artisan:storage:link', // |
+    'artisan:view:cache',   // |
+    'artisan:config:cache', // | Laravel specific steps 
+    'artisan:optimize',     // |
+    // 'artisan:migrate',      // |
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',
